60f1a8ca7c4c8e998f6a350c41421fade721fa19
yue-studio <47492421+yue-studio@users.noreply.github.com>
2022-02-28 15:36:15 -0800

    Create subset4.c

---
+++ subset4.c
@@ -0,0 +1,25 @@
+#bitmask method in C
+
+#include <stdio.h>
+
+void subset(int *nums, int comb) {
+    for (int i = 0; i < 16; i++ ) {
+        if (comb & (1u << i)) {
+           printf("%d", nums[i]);
+        }
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int numlist[3] ={1, 2, 3};
+    size_t n = sizeof(numlist)/sizeof(numlist[0]);
+    int noSubsets = (int) pow((double) 2, n);
+
+    for (int i=1; i< noSubsets;i++) {
+         subset(numlist, i);
+    }
+
+    return 0;
+}
